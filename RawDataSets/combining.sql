4da6626542a90420d334773e34cf6319d3f4ed77
Shawn Cavazos <scavazos4@gmail.com>
2021-06-14 12:14:05 -0500

    combined data sets

---
+++ RawDataSets/combining.sql
@@ -0,0 +1,43 @@
+Create table ratings (
+tconst varchar,
+averageRating float,
+numVotes integer
+);
+
+create table title (
+tconst varchar,
+titleType varchar,
+primaryTitle char,
+originalTitle char,
+isAdult	boolean,
+startYear integer,
+endYear	varchar,
+runtimeMinutes integer,
+genres varchar
+);
+
+
+create table neflix (
+show_id	varchar,
+type varchar,
+title varchar,
+director varchar,
+cast varchar,
+country varchar,	
+country varchar,	
+country	varchar,
+YEAR_added integer,
+release_year integer,
+rating varchar,
+duration varchar,
+TYPE1/listed_in varchar,
+TYPE2 varchar,
+TYPE3 varchar,	
+description varchar
+);
+
+
+select * from title
+
+drop table title
+
