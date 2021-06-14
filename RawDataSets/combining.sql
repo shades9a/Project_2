6e1cdbce0e2fffc71da69181939e47c51dc8f098
Shawn Cavazos <scavazos4@gmail.com>
2021-06-14 17:55:11 -0500

    note

--- RawDataSets/combining.sql
+++ RawDataSets/combining.sql
@@ -1,43 +1,26 @@
-Create table ratings (
-tconst varchar,
-averageRating float,
-numVotes integer
-);
-
-create table title (
-tconst varchar,
-titleType varchar,
-primaryTitle char,
-originalTitle char,
-isAdult	boolean,
-startYear integer,
-endYear	varchar,
-runtimeMinutes integer,
-genres varchar
-);
-
-
-create table neflix (
-show_id	varchar,
-type varchar,
+create table shows (
 title varchar,
-director varchar,
-cast varchar,
-country varchar,	
-country varchar,	
-country	varchar,
+country varchar,
 YEAR_added integer,
 release_year integer,
 rating varchar,
-duration varchar,
-TYPE1/listed_in varchar,
-TYPE2 varchar,
-TYPE3 varchar,	
-description varchar
+duration varchar,	
+TYPE1 varchar
+);
+
+create table ratings (
+title varchar,
+rating varchar,
+user_rating_score varchar
 );
 
 
-select * from title
+select * from shows
 
-drop table title
+create table combo as
+select shows.title, shows.country, shows.year_added, shows.release_year, shows.rating, shows.type1, ratings.user_rating_score
+from shows
+inner join ratings on shows.title = ratings.title;
+
+select * from combo 
 
